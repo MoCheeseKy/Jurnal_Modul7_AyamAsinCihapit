fa86c39972b3f229cd3d313380c4d49255433535
Vastuolu <muhamadnaufalnabillansyah@gmail.com>
2026-04-18 14:18:33 +0700

    menambahkan class FilmFavorit_103022400073

---
+++ FilmFavorit_103022400073.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace Jurnal_Modul7_AyamAsinCihapit
+{
+    // "title": "Kamu di Adopsi",
+    //"director": "Mulyani",
+    //"year": "2077",
+    //"genre": "Drama",
+    //"rating": 4.9,
+    //"durationMinutes": 250,
+    //"isWatched": true
+    internal class FilmFavorit_103022400073
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public float rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+
+        public static void ReadJson()
+        {
+            string filePath = "../../../jurnal7_1_103022400073.json";
+            string jsonString = File.ReadAllText(filePath);
+
+            FilmFavorit_103022400073 filmFav = JsonSerializer.Deserialize<FilmFavorit_103022400073>(jsonString);
+
+            Console.WriteLine($"{filmFav.title} | {filmFav.genre} ({filmFav.year} - {filmFav.rating}) {filmFav.durationMinutes} minutes");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FilmFavorit_103022400073.ReadJson();
         }
     }
 }
