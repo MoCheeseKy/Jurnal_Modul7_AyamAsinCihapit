c1c39f0b105c339f34a9292ec53251c2fcc8b26b
RizqieSufiawan <iamrizqie@gmail.com>
2026-04-18 14:50:25 +0700

    Menambahkan Class FILMFAVORIT

---
+++ FilmFavorit_103022400069.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Jurnal_Modul7_AyamAsinCihapit
+{
+    internal class FilmFavorit_103022400069
+    {
+        public void ReadJSON()  
+        {
+            string filePath = "jurnal7_1_103022400069.json";
+            string jsonString = File.ReadAllText(filePath);
+            Film flm = JsonSerializer.Deserialize<Film>(jsonString);
+            Console.WriteLine("Title "+ flm.title + ", Director " + flm.director + " , Year " +flm.year+ " , Genre "+ flm.genre+ " , Rating " + flm.rating + "/10 , Duration "+flm.durationMinutes+" Min , Watched ? "+flm.isWatched);
+        }
+    }
+    public class Film
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FilmFavorit_103022400069 film = new FilmFavorit_103022400069();
+                film.ReadJSON();
         }
     }
 }
