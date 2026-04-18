1b0730840dc4bc32ca37811ebad584ff506bc5f4
unknown <thoriqabdurrohman@student.telkomuniversity.ac.id>
2026-04-18 13:49:39 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jurnal_Modul7_AyamAsinCihapit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
