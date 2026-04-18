8436e123fa31cc57726f4d3639f8cc7a44975f3b
unknown <rifky.muhammadprayudhi@gmail.com>
2026-04-18 14:45:18 +0700

    fix typo commit 1

--- FilmFavorit_103022400003.cs
+++ FilmFavorit_103022400003.cs
@@ -7,26 +7,26 @@ namespace Jurnal_Modul7_AyamAsinCihapit
 {
     internal class FilmFavorit_103022400003
     {
-        public string Title { get; set; }
-        public string Director { get; set; }
-        public string Year { get; set; }
-        public string Genre { get; set; }
-        public float Rating { get; set; }
-        public int DurationMinutes { get; set; }
-        public bool IsWatched { get; set; } = false;    
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public float rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; } = false;    
 
         public static void ReadJSON()
         {
             string json = File.ReadAllText("jurnal7_1_103022400003.json");
             FilmFavorit_103022400003 film = JsonSerializer.Deserialize<FilmFavorit_103022400003>(json);
             Console.WriteLine(
-                $"Title: {film.Title}, " +
-                $"Director: {film.Director}, " +
-                $"Year: {film.Year}, " +
-                $"Genre: {film.Genre}, " +
-                $"Rating: {film.Rating}, " +
-                $"DurationMinutes: {film.DurationMinutes}, " +
-                $"IsWatched: {film.IsWatched}");
+                $"Title: {film.title}, " +
+                $"Director: {film.director}, " +
+                $"Year: {film.year}, " +
+                $"Genre: {film.genre}, " +
+                $"Rating: {film.rating}, " +
+                $"DurationMinutes: {film.durationMinutes}, " +
+                $"IsWatched: {film.isWatched}");
 
         }
     }
