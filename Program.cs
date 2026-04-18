fee657d417f6d70cbca2cb760480c4ca2e666545
HemiCeLL <maxthejanitor360@gmail.com>
2026-04-18 15:17:09 +0700

    nambah kelas WatchList_130202434015

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@
 FilmFavorit_103022430015 ff = new FilmFavorit_103022430015();
 ff.ReadJSON();
 
-
+Watchlist_103022430015 ww = new Watchlist_103022430015();
+ww.ReadJSON();
 
---
+++ Watchlist_103022430015.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Jurnal_Modul7_AyamAsinCihapit
+{
+    internal class Movie
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public string genre { get; set; }
+        public double rating { get; set; }
+
+    }
+    internal class watchlistData
+    {
+        public string watchlistName { get; set; }
+        public string createdBy { get; set; }
+        public List<Movie> movies { get; set; }
+    }
+
+    internal class Watchlist_103022430015
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("jurnal7_2_103022430015.json");
+            watchlistData data = JsonSerializer.Deserialize<watchlistData>(jsonString);
+
+            Console.WriteLine("Watchlist\n");
+            Console.WriteLine("Watchlist name: " + data.watchlistName);
+            Console.WriteLine(" ");
+            Console.WriteLine("created by : " + data.createdBy);
+            Console.WriteLine(" ");
+            Console.WriteLine("Movies : ");
+
+            for (int i = 0; i < data.movies.Count; i++)
+            {
+                Console.WriteLine(data.movies[i].id + " " +  data.movies[i].title + " " + data.movies[i].year + " " + data.movies[i].rating);
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
