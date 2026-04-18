e19c132348a1d7e841f4ddae159824cd856d679c
unknown <zulfanfalah@gmail.com>
2026-04-18 15:02:47 +0700

    menambahkan class Watchlist_103022400076

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿
+﻿Console.WriteLine("Deserialization 1:\n");
+
         FilmFavorit_103022400076 mhs = new FilmFavorit_103022400076();
         mhs.readJSON();
+
+Console.WriteLine("\n__________\nDeserialization 2:\n");
+        Watchlist_103022400076 wth  = new Watchlist_103022400076();
+        wth.ReadJSON();
